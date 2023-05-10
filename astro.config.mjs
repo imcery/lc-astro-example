6afc6fa90d34a1a190a984b06309ee3bdce23609
Pavel Saparov <saparov.p@gmail.com>
2023-05-10 14:50:53 +0200

    Astro Example

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+import vue from "@astrojs/vue";
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  trailingSlash: "ignore",
+  integrations: [react(), vue(), svelte(), tailwind()],
+});
